6875f854ec23523350ddb3c63714114f48269fa4
Brenda Schapievski <brendaschapievski.data@gmail.com>
2024-01-05 14:16:23 -0300

    Inserido código Python até a extração de pokemon e detalhes

--- pokemon.py
+++ pokemon.py
@@ -2,6 +2,7 @@ import requests
 import json
 
 url = "https://pokeapi.co/api/v2/pokemon/"
+pokemon_list = list()
 
 while url != None:
     payload = {}
@@ -12,7 +13,12 @@ while url != None:
 
     for item in response["results"]:
        pokemon_name = item["name"]
-       url_pokemon = f "https://pokeapi.co/api/v2/pokemon/{pokemon_name}"
+       url_pokemon = f"https://pokeapi.co/api/v2/pokemon/{pokemon_name}"
        response_pokemon = json.loads(requests.request("GET", url_pokemon, headers=headers, data=payload).text)
 
-       
+       pokemon_list.append(response_pokemon)
+       print(response_pokemon["id"])
+
+
+print(pokemon_list)
+
