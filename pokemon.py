c8c3189535ab20494449d83caeb32f879ac19c27
Brenda Schapievski <brendaschapievski.data@gmail.com>
2024-01-06 11:13:04 -0300

    Inserido arquivos json

--- pokemon.py
+++ pokemon.py
@@ -4,6 +4,8 @@ import json
 url = "https://pokeapi.co/api/v2/pokemon/"
 pokemon_list = list()
 
+counter = 1
+
 while url != None:
     payload = {}
     headers = {}
@@ -12,20 +14,22 @@ while url != None:
     url = response ["next"]
 
     for item in response["results"]:
-       pokemon_name = item["name"]
-       url_pokemon = f"https://pokeapi.co/api/v2/pokemon/{pokemon_name}"
-       response_pokemon = json.loads(requests.request("GET", url_pokemon, headers=headers, data=payload).text)
+        pokemon_name = item["name"]
+        url_pokemon = f"https://pokeapi.co/api/v2/pokemon/{pokemon_name}"
+        response_pokemon = json.loads(requests.request("GET", url_pokemon, headers=headers, data=payload).text)
 
-       pokemon_list.append(response_pokemon)
-       print(response_pokemon["id"])
+        pokemon_list.append(response_pokemon)
+        print(response_pokemon["id"])
 
+    file_path = fr"C:\Users\eolen\OneDrive\Área de Trabalho\PokeDados\pokemon_files\pokemon_file_{counter}.json"
+
+    with open(file_path, "w") as outfile: 
+        print(f"Salvando arquivo em: {file_path}")
+        json.dump(pokemon_list, outfile)
+
+    outfile.close()
+    counter = counter + 1
+    pokemon_list = list()
 
 print(pokemon_list)
 
-file_path = r"C:\Users\eolen\OneDrive\Área de Trabalho\PokeDados\pokemon_files\pokemon_list.json"
-
-with open(file_path, "w") as outfile: 
-    print(f"Salvando arquivo em: {file_path}")
-    json.dump(pokemon_list, outfile)
-
-outfile.close()
